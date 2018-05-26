bb708c9cf4ddb73a435987023064009ec27cfed0
Luan Phan <luan.phan@go1.com>
2018-05-26 14:11:37 +0700

    update font-awesome

--- src/vendor.ts
+++ src/vendor.ts
@@ -18,3 +18,4 @@ import 'rxjs-compat';
 // Other vendors for example jQuery, Lodash or Bootstrap
 // You can import js, ts, css, sass, ...
 import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.css';
