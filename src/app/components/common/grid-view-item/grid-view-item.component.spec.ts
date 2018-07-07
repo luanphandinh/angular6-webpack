6c6fd3a1f489a16c4def58a465db754e80675e0c
Luan Phan <luan.phan@go1.com>
2018-07-07 15:01:45 +0700

    update ts-lint

--- src/app/components/common/grid-view-item/grid-view-item.component.spec.ts
+++ src/app/components/common/grid-view-item/grid-view-item.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import {Component, NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
 
 import { GridViewItemComponent } from './grid-view-item.component';
@@ -10,12 +10,12 @@ import { GridViewItemDirective } from './grid-view-item.directive';
   selector: 'go1-testing-component',
   template: '<div></div>',
 })
-class TestingComponent {}
+class TestingComponent { }
 @NgModule({
   declarations: [TestingComponent],
   imports: [BrowserDynamicTestingModule],
   entryComponents: [TestingComponent],
-  exports: [TestingComponent]
+  exports: [TestingComponent],
 })
 class TestingModule {
 }
@@ -23,20 +23,20 @@ class TestingModule {
 describe('GridViewItemComponent', () => {
   let component: GridViewItemComponent;
   let fixture: ComponentFixture<GridViewItemComponent>;
-  const viewContainerRef = {clear: () => {}, createComponent: () => ({instance: {}})} as any;
+  const viewContainerRef = { clear: () => { }, createComponent: () => ({ instance: {} }) } as any;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [ TestingModule],
-      declarations: [ GridViewItemComponent ]
+      imports: [TestingModule],
+      declarations: [GridViewItemComponent],
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(GridViewItemComponent);
     component = fixture.componentInstance;
-    component.item = new GridViewItem(TestingComponent, {'key': 'value'});
+    component.item = new GridViewItem(TestingComponent, { key: 'value' });
     component.layout = 'grid';
     component.go1ViewItemHost = new GridViewItemDirective(viewContainerRef);
     fixture.detectChanges();
